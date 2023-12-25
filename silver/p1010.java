c5feb9fedb43c45038db784ec32210c808f9e71f
sigema0223 <sigema0223@gmail.com>
2023-12-25 09:45:49 +0000

    salmon

---
+++ silver/p1010.java
@@ -0,0 +1,24 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+// can't solve
+
+public class p1010 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = Integer.parseInt(st.nextToken());
+
+        for (int i=0; i<num; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+
+        }
+    }
+}
