b2f50d20de2c04abb189b136fe6ab42747985547
sigema0223 <sigema0223@gmail.com>
2023-12-28 13:54:02 +0000

    salmon

---
+++ silver/p1094.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class p1094{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int finalLength = Integer.parseInt(br.readLine());
+        int initialLength = 64, count = 0;
+
+        while(finalLength>0){
+            if(initialLength>finalLength){
+                initialLength/=2;
+            }else{
+                finalLength-=initialLength;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
