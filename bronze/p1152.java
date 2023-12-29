9873cd7b8c49f44078b52a6d0c64db3b200c33ce
sigema0223 <sigema0223@gmail.com>
2023-12-29 07:09:45 +0000

    salmon

---
+++ bronze/p1152.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class p1152 {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		System.out.print(st.countTokens());
+	}
+}
