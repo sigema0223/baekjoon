29e66af71a4cfde0657d15ff138b23d5ae272b51
sigema0223 <sigema0223@gmail.com>
2024-01-04 13:25:01 +0000

    salmon

--- bronze/P1292.java
+++ bronze/P1292.java
@@ -3,6 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+public class P1292{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+        int count = 0;
+
+        for(int i = 1; i <= end; i++){
+            for(int j = 1; j <= i; j++){
+                count++;
+                if(start<=count && count<=end){
+                    sum+=i;
+                }
+            }     
+        }
+        System.out.println(sum);
+    }
+}
+
+/* 2번째 방법 : 전체 배열 생성 후 원하는 구간 합 구하기
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class trash {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,3 +57,4 @@ public class trash {
         System.out.println(sum);
     }
 }
+ */
