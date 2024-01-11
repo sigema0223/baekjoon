1292e6e049cd71d7849aa08e71f16668f29f1aea
sigema0223 <sigema0223@gmail.com>
2024-01-11 13:04:52 +0000

    salmon

--- trash.java
+++ trash.java
@@ -1,14 +1,15 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.IOException;
+ 
 public class trash {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int t = Integer.parseInt(st.nextToken());
-
-        System.out.println(t-543);
-    }
+ 
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int y = Integer.parseInt(br.readLine());
+		
+		System.out.print((y%4==0)?((y%400==0)?"1":(y%100==0)?"0":"1"):"0");
+	}
 }
