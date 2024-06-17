f49250d224f9fccf192bf81bc8a660fef858d21b
sigema0223 <sigema0223@gmail.com>
2024-06-17 07:17:59 +0000

    salmon

--- bronze/P1551.java
+++ bronze/P1551.java
@@ -1,16 +1,13 @@
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.IOException;
 
-public class P1551{
+public class P1551 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());;
+        String[] firstLine = br.readLine().split(" ");
+        int K = Integer.parseInt(firstLine[0]);
+        int N = Integer.parseInt(firstLine[1]);
 
         String[] str = br.readLine().split(",");
         int[] arr = new int[K];
@@ -18,14 +15,22 @@ public class P1551{
             arr[i] = Integer.parseInt(str[i]);
         }
 
-        int[] result = new int[K];
+        for (int j = 0; j < N; j++) {
+            int[] temp = new int[K - 1 - j]; // 매번 크기가 줄어드는 배열 생성
+            for (int i = 0; i < K - 1 - j; i++) {
+                temp[i] = arr[i + 1] - arr[i];
+            }
+            arr = temp; // arr 배열을 temp 배열로 업데이트
+        }
 
-        for(int j=0;j<N;j++){
-            for(int i=0;i<K-1;i++){
-                result[i]=arr[i+1]-arr[i];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+            if (i < arr.length - 1) {
+                sb.append(",");
             }
         }
-        
-        for(int i=0;i<K-N;i++)  System.out.println(result[i]);
+
+        System.out.println(sb.toString());
     }
 }
